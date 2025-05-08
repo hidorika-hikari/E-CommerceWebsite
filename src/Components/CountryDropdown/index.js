633825204e7583db212448beaab3ea9852a8e2f8
hidorika-hikari <promvet_khiawsa@cmu.ac.th>
2025-05-08 13:45:57 +0700

    Finish #6

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,16 +1,61 @@
+import React from 'react';
 import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog';
+import { useState } from 'react';
 import { FaAngleDown } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
+import { MdClose } from "react-icons/md";
+import Slide from '@mui/material/Slide';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+  
 
 const CountryDropdown =() => {
+
+    const [isOpenModel, setisOpenModel] = useState(false)
+
     return(
         <>
-        <Button className='countryDrop'>
+        <Button className='countryDrop' onClick={()=>setisOpenModel(true)}>
             <div className='info d-flex flex-column'>
                 <span className='label'>Your Location</span>
                 <span className='name'>India</span>
             </div>
             <span className='ms-auto'><FaAngleDown/></span>
         </Button>
+
+        <Dialog open={isOpenModel} onClose={()=>setisOpenModel(false)} className='locationModel' TransitionComponent={Transition}>
+            <h4 className='mb-0'>Choose your Delivery Location</h4>
+            <p>Enter your address and we'll specify the offer for your area.</p>
+            <Button className='close_' onClick={()=>setisOpenModel(false)}><MdClose/></Button>
+
+            <div className='headerSearch w-100'>
+                <input type='text' placeholder='Search your area...'/>
+                <Button><IoIosSearch/></Button>
+            </div>
+
+            <ul className='countryList mt-3'>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>Thailand</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+                <li><Button onClick={()=>setisOpenModel(false)}>India</Button></li>
+            </ul>
+        </Dialog>
+
         </>
     )
 }
