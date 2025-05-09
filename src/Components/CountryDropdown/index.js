2432bf7808d508cfa55e5e2a3096e24c53cfc2f5
hidorika-hikari <promvet_khiawsa@cmu.ac.th>
2025-05-09 15:07:06 +0700

    setcountryList(context.countryList)

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -16,9 +16,33 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 const CountryDropdown =() => {
 
-    const [isOpenModel, setisOpenModel] = useState(false)
+    const [isOpenModel, setisOpenModel] = useState(false);
+    const [selectedTab, setsetlectTab] = useState(null);
+    const [countryList, setcountryList] = useState([]);
 
     const context = useContext(MyContext);
+
+    const selectCountry = (index) =>{
+        setsetlectTab(index);
+        setisOpenModel(false);
+    }
+
+    useEffect(()=>{
+        setcountryList(context.countryList);
+    },[])
+
+    const filterList=(e) =>{
+        const keyword = e.target.value.toLowerCase();
+
+        if (keyword !== ""){
+            const list = countryList.filter((item) =>{
+                return item.country.toLowerCase().includes(keyword);
+            });
+            setcountryList(list);
+        } else {
+            setcountryList(context.countryList);
+        }
+    }
 
     return(
         <>
@@ -36,15 +60,17 @@ const CountryDropdown =() => {
             <Button className='close_' onClick={()=>setisOpenModel(false)}><MdClose/></Button>
 
             <div className='headerSearch w-100'>
-                <input type='text' placeholder='Search your area...'/>
+                <input type='text' placeholder='Search your area...' onChange={filterList}/>
                 <Button><IoIosSearch/></Button>
             </div>
 
             <ul className='countryList mt-3'>
             {
-                context.countryList?.length!==0 && context.countryList?.map((item,index)=>{
+                countryList?.length!==0 && countryList?.map((item,index)=>{
                     return(
-                        <li key={index}><Button onClick={()=>setisOpenModel(false)}>{item.country}</Button></li>
+                        <li key={index}><Button onClick={()=>selectCountry(index)} 
+                        className={`${selectedTab === index ? 'active' : ''}`}
+                        >{item.country}</Button></li>
                     )
                 })
             }
